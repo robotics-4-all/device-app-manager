ec553dbd258d2e215f0e8373905b76cc487428c3
Konstantinos Panayiotou <klpanagi@gmail.com>
2021-07-16 23:35:39 +0300

    Cleanup

--- examples/rapid_delete_app.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-import sys
-import os
-
-os.system(f"./delete_app.py --device-id {sys.argv[1]} --host 155.207.33.189 --port 5782 --vhost / --debug --app-id {sys.argv[2]}")
--- examples/rapid_get_apps.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-import sys
-import os
-
-os.system(f"./get_apps.py --device-id {sys.argv[1]} --host r4a-platform.ddns.net --port 5782 --vhost / --debug")
