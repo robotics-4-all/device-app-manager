0aa1e26f3bf1b260d3f404aaf0e9da03b1c9630d
Konstantinos Panayiotou <klpanagi@gmail.com>
2021-11-10 20:09:58 +0200

    Fixes parameter loading bugs

--- bin/app_manager.py
+++ bin/app_manager.py
@@ -1,12 +1,5 @@
 #!/usr/bin/env python
 
-from __future__ import (
-    absolute_import,
-    division,
-    print_function,
-    unicode_literals
-)
-
 import sys
 import os
 import json
@@ -17,64 +10,23 @@ from device_app_manager import AppManager, load_cfg
 
 def main():
     parser = argparse.ArgumentParser(description='Application Manager CLI')
-    parser.add_argument('--host', dest='host',
-                        help='AMQP broker host (IP/Hostname)',
-                        default=None)
-    parser.add_argument('--port', dest='port',
-                        help='AMQP broker listening port',
-                        default=None)
-    parser.add_argument('--vhost', dest='vhost',
-                        help='Virtual host to connect to',
-                        default=None)
-    parser.add_argument('--username', dest='username',
-                        help='Authentication username',
-                        default=None)
-    parser.add_argument('--password', dest='password',
-                        help='Authentication password',
-                        default=None)
-    parser.add_argument('--queue-size', dest='queue_size',
-                        help='Maximum queue size.',
-                        type=int,
-                        default=None)
-    parser.add_argument('--heartbeat', dest='heartbeat',
-                        help='Heartbeat interval in seconds',
-                        type=int,
-                        default=None)
     parser.add_argument('--config', dest='config',
                         help='Config file path',
                         default='~/.config/device_app_manager/config')
-    parser.add_argument('--debug', dest='debug',
-                        help='Enable debugging',
-                        type=bool,
-                        const=True,
-                        nargs='?')
+    # parser.add_argument('--debug', dest='debug',
+    #                     help='Enable debugging',
+    #                     type=bool,
+    #                     const=True,
+    #                     nargs='?')
 
     args = parser.parse_args()
     config_file = args.config
-    username = args.username
-    password = args.password
-    host = args.host
-    port = args.port
-    vhost = args.vhost
-    debug = args.debug
-    heartbeat = args.heartbeat
+    # debug = args.debug
 
     config = load_cfg(config_file)
 
     ## Parameters passed from CLI are getting priority and override
     ## those defined in the configuration file
-    if username is not None:
-        config['platform_broker']['username'] = username
-    if password is not None:
-        config['platform_broker']['password'] = password
-    if host is not None:
-        config['platform_broker']['host'] = host
-    if port is not None:
-        config['platform_broker']['port'] = port
-    if vhost is not None:
-        config['platform_broker']['vhost'] = vhost
-    if heartbeat is not None:
-        config['heartbeat_interval'] = heartbeat
 
     print('==================== AppManager Configuration ====================')
     print(json.dumps(config, indent=4, sort_keys=True))
